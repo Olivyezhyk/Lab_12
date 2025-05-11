d3cda9fa578187693c86e4ba30ca4f2d85903882
Olivyezhyk <olegsusyak3101@gmail.com>
2025-05-11 19:46:28 +0300

    First Commit

--- Lab_12_01/Lab_12_01.cpp
+++ Lab_12_01/Lab_12_01.cpp
@@ -17,23 +17,19 @@ int main() {
     p->link1 = new Elem;
     p->link1->info = 2;
 
+    p->link1->link1 = p;
+    p->link1->link2 = new Elem;
+
+    p->link1->link2->info = 3;
+    p->link1->link2->link1 = p->link1;
+
     p->link2 = new Elem;
     p->link2->info = 4;
 
-    p->link1->link1 = p;
-    p->link1->link2 = new Elem;
-    p->link1->link2->info = 3;
+    p->link2->link2 = NULL;
+    p->link1->link2->link2 = p->link2->link2;
 
-    p->link1->link2->link1 = p->link1;
-    p->link1->link2->link2 = p->link2;
-
-    p->link2->link1 = p;
-    p->link2->link2 = p->link1;
-
-    cout << p->info << endl;
-    cout << p->link1->info << endl;
-    cout << p->link1->link2->info << endl;
-    cout << p->link1->link2->link2->info << endl;
+    p->link2->link1 = p->link1;
 
     delete p->link1->link2;
     delete p->link1;       
